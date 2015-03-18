292db8db968f129a9786978f96d3fe41332a453e
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2015-03-18 22:48:47 +0900

    make count app

--- Count/app/src/main/java/com/example/ucucair2/count/MainActivity.java
+++ Count/app/src/main/java/com/example/ucucair2/count/MainActivity.java
@@ -4,16 +4,26 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
+    TextView text;
+    int number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = (TextView)findViewById(R.id.textView);
+        text.setText("0");
     }
 
+    public void plus(View v) {
+        number = number + 1;
+        text.setText(String.valueOf(number));
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
