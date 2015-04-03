9d47e9b3b9c01cef1c12476766333d5117011425
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2015-04-04 00:08:08 +0900

    update clap

--- Clap/app/src/main/java/com/example/ucucair2/clap/Clap.java
+++ Clap/app/src/main/java/com/example/ucucair2/clap/Clap.java
@@ -1,7 +1,35 @@
 package com.example.ucucair2.clap;
 
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.SoundPool;
+
 /**
  * Created by ucucAir2 on 15/04/02.
  */
 public class Clap {
+    private SoundPool soundPool;
+    private int soundId;
+
+    public Clap(Context context) {
+        soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+        soundId = soundPool.load(context, R.raw.clap, 1);
+    }
+
+    public void play() {
+        soundPool.play(soundId, 1.0f, 1.0f, 1, 0, 1.0f);
+    }
+
+    public void repeatPlay(int repeat) {
+        int count = 0;
+        while (count < repeat) {
+            play();
+            count++;
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- Clap/app/src/main/java/com/example/ucucair2/clap/MainActivity.java
+++ Clap/app/src/main/java/com/example/ucucair2/clap/MainActivity.java
@@ -1,19 +1,67 @@
 package com.example.ucucair2.clap;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.Spinner;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity implements AdapterView.OnItemSelectedListener {
+    ImageButton button;
+    Clap clap;
+    Spinner spinner;
+    int repeat = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (ImageButton)findViewById(R.id.imgBtn);
+        spinner = (Spinner)findViewById(R.id.spinner);
+
+        clap = new Clap(this.getApplicationContext());
+        clap.play();
+
+        String[] strArray = new String[5];
+        strArray[0] = "パンッ！";
+        strArray[1] = "パンッパンッ！！";
+        strArray[2] = "パンッパンッパンッ！！";
+        strArray[3] = "4回";
+        strArray[4] = "5回";
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+            this, android.R.layout.simple_spinner_item, strArray
+        );
+        arrayAdapter.setDropDownViewResource(
+            android.R.layout.simple_spinner_dropdown_item
+        );
+        spinner.setAdapter(arrayAdapter);
+        spinner.setOnItemSelectedListener(this);
     }
 
+    public void onButtonPushed(View v) {
+        Log.v("tag", "message message");
+        clap.repeatPlay(repeat);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View v, int pos, long id){
+        repeat = pos+1;
+        Log.v("tag", "message");
+        clap.play();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
