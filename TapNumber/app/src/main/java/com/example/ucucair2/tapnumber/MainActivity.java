672338be81797c7a669f5650a658036241014bd0
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2015-04-07 15:16:15 +0900

    update

--- TapNumber/app/src/main/java/com/example/ucucair2/tapnumber/MainActivity.java
+++ TapNumber/app/src/main/java/com/example/ucucair2/tapnumber/MainActivity.java
@@ -1,19 +1,90 @@
 package com.example.ucucair2.tapnumber;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    int[] array;
+    String question;
+    int correctCounter;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView)findViewById(R.id.textView);
+        start();
     }
 
+    public void start() {
+        array = new int[4];
+        Random rand = new Random();
+        array[0] = rand.nextInt(4)+1;
+        array[1] = rand.nextInt(4)+1;
+        array[2] = rand.nextInt(4)+1;
+        array[3] = rand.nextInt(4)+1;
+
+        question = Integer.toString(array[0]) + Integer.toString(array[1]) + Integer.toString(array[2]) + Integer.toString(array[3]);
+        textView.setText(question);
+        correctCounter = 0;
+    }
+
+    public void endCheck () {
+        if (correctCounter == 4) {
+            start();
+        }
+    }
+
+    public void number1 (View v) {
+        if (array[correctCounter] == 1) {
+            question = question.substring(1);
+            textView.setText(question);
+            correctCounter++;
+            endCheck();
+        } else {
+            Toast.makeText(this, "数字が違うよ", Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void number2 (View v) {
+        if (array[correctCounter] == 2) {
+            question = question.substring(1);
+            textView.setText(question);
+            correctCounter++;
+            endCheck();
+        } else {
+            Toast.makeText(this, "数字が違うよ", Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void number3 (View v) {
+        if (array[correctCounter] == 3) {
+            question = question.substring(1);
+            textView.setText(question);
+            correctCounter++;
+            endCheck();
+        } else {
+            Toast.makeText(this, "数字が違うよ", Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void number4 (View v) {
+        if (array[correctCounter] == 4) {
+            question = question.substring(1);
+            textView.setText(question);
+            correctCounter++;
+            endCheck();
+        } else {
+            Toast.makeText(this, "数字が違うよ", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
