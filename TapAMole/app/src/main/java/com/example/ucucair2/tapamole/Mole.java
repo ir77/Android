3e580b96f1611cbf2626e945a105221e5dd5dcae
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2015-04-09 11:28:51 +0900

    update

---
+++ TapAMole/app/src/main/java/com/example/ucucair2/tapamole/Mole.java
@@ -0,0 +1,12 @@
+package com.example.ucucair2.tapamole;
+
+import android.widget.ImageView;
+
+/**
+ * Created by ucucAir2 on 15/04/09.
+ */
+public class Mole {
+    int state;  // もぐらの状態 0:潜っている 1:出てきている 2:叩かれている
+    ImageView moleImage;
+    
+}
